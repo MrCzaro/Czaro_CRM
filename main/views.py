e36a0460f47f4e63c0e75da2b30b8816667d24d5
Mr.Czaro <cezary.tubacki@gmail.com>
2024-01-17 07:42:58 +0100

    Commit 2 17/1/24

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib import messages
+def main(request):
+    context = {
+        "title": "Main Page",
+    }
+    messages.success(request, "Just Checking messages!")
+    return render(request, "base.html", context)
