2a3c823b7d42598096571e8f8608c775eba3bd23
Mr.Czaro <cezary.tubacki@gmail.com>
2024-01-22 16:25:13 +0100

    commit 22/1/24

--- patient/models.py
+++ patient/models.py
@@ -33,6 +33,18 @@ class Patient(models.Model):
     consent_contact_number = models.CharField(max_length=15, blank=True)
     
    
-    discharged_on = models.DateTimeField(blank=True)
+    discharged_on = models.DateTimeField(blank=True, null=True)
+    is_discharged = models.BooleanField(default=False)
     def __str__(self):
        return f"{self.first_name} {self.last_name}"
+   
+class PatientObservation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient = models.ForeignKey(Patient, related_name = "observations", on_delete=models.CASCADE)
+    observation = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ("-created_at",)
